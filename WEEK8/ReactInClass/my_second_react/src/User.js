1867e8555f6791fce211b7afe009029884b8decd
Rusty Nail <87020554+rustynail77@users.noreply.github.com>
2022-05-08 12:58:54 +0300

    test with gitignore file

---
+++ WEEK8/ReactInClass/my_second_react/src/User.js
@@ -0,0 +1,11 @@
+const User = () => {
+    return (
+        <div>
+            <h2>User info:</h2>
+            <h4>User name:</h4>
+            <p>Username</p>
+            <p>email</p>
+        </div>
+    )
+}
+export default User;
